f5b08403d122c3cf3e8b3a92f6c34e13d6fbaa6d
Simon <aguilarsimon01@gmail.com>
2019-12-29 15:36:48 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* React: Basics */
+function ListItem(props) {
+    return (
+      <li onClick={props.onClick}>
+        {props.item}
+      </li>
+    );
+  }
+  
+  class ShoppingList extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        list: ['unicycle', 'juggling clubs', 'stilts']
+      };
+    }
 
-export default App;
+    addItem() {
+      var item = document.getElementById("listItem").value;
+      document.getElementById("listItem").value = "";
+      var newList = this.state.list.slice();
+      newList.push(item);
+      this.setState({list: newList});
+    }  
+    
+    enterPressed(event) {
+      var code = event.keyCode || event.which;
+      if(code === 13) { //13 is the enter keycode
+        var item = document.getElementById("listItem").value;
+        document.getElementById("listItem").value = "";
+        var newList = this.state.list.slice();
+        newList.push(item);
+        this.setState({list: newList});
+      } 
+  }
+    
+    onClick(index) {
+      var newList = this.state.list.slice();
+      newList.splice(index, 1);
+      this.setState({list: newList});
+    }
+    
+    render() {
+      var listItems = [];
+      this.state.list.forEach((item, i) => {
+        listItems.push(<ListItem item={item} onClick={() => this.onClick(i)} />)
+      });
+      return (
+        <div className="shopping-list">
+          <h1>Shopping List</h1>
+           <input 
+              type="text"
+              id="listItem" 
+              placeholder="Add item"
+              onKeyPress={this.enterPressed.bind(this)}
+          />
+          <button className='btn'type="button" onClick={() => this.addItem()}>Add</button>
+          <ul className='listItems'>
+            {listItems}
+          </ul>
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <ShoppingList />,
+    document.getElementById('root')
+  );
+
+export default ShoppingList;
